520f448be34bd05418645412ca48776c01487c44
Mary Jacobs <TheMaryJacobs@gmail.com>
2019-11-19 19:52:58 -0600

    separate questions.js file - update later

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+let questions = [
+  {
+      question : "What does HTML stand for?",
+      imgSrc : "img/html.png",
+      choiceA : "Correct",
+      choiceB : "Wrong",
+      choiceC : "Wrong",
+      choiceD : "Yes it is",
+      correct : "A"
+  },{
+      question : "What does CSS stand for?",
+      imgSrc : "img/css.png",
+      choiceA : "Wrong",
+      choiceB : "Correct",
+      choiceC : "Wrong",
+      choiceD : "Yes it is",
+      correct : "B"
+  },{
+      question : "What does JS stand for?",
+      imgSrc : "img/js.png",
+      choiceA : "Wrong",
+      choiceB : "Wrong",
+      choiceC : "Correct",
+      choiceD : "Yes it is",
+      correct : "C"
+  }
+];
